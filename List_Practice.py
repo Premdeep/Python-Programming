353881000456ac58b7f618501ff9eb19b7d3261f
Premdeep Amudala <preamdhep.usapp@gmail.com>
2016-01-09 15:45:21 -0800

    Lists practice

---
+++ List_Practice.py
@@ -0,0 +1,26 @@
+# List Practice
+s = [0,1,2,3,4,5,6,7,8]
+
+# prints first element
+print (s[0])
+
+# prints last element
+print (s[-1]) 
+
+# prints list with out last element
+print (s[:-1])
+
+# prints list only with last element
+print (s[-1:]) 
+
+# prints whole list
+print (s[:]) 
+
+# prints reverse list
+print (s[::-1])
+
+#prints elements in a list at particular index: 2 in this case
+print (s[::2])
+
+#prints slice of list with one less element
+print (s[:3])
